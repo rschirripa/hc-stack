1ce64849bd5b0a23e927ab09d354d039cd84e4cf
gcastill0 <gilberto.castillo@rogers.com>
2019-03-11 15:35:30 -0400

    Initial Azure template v 0.1

--- azure/main.tf
+++ azure/main.tf
@@ -1,2 +1,77 @@
-[]
+######### ######### ######### ######### ######### ######### ######### #########
 
+variable "prefix" {
+  description = "interrupt-test"
+  default = "interrupt-test"
+}
+
+variable "location" {
+  description = "US East"
+  default = "eastus"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {
+                    environment = "test",
+                    owner = "gcatill0"
+                }
+  description = "Basic tags"
+}
+
+######### ######### ######### ######### ######### ######### ######### #########
+
+locals {
+  virtual_machine_name = "${var.prefix}-vm"
+  admin_username       = "testadmin"
+  admin_password       = "Password1234!"
+}
+
+# Azure Provider
+provider "azurerm" {
+  version = "=1.22.0"
+  subscription_id = "0911d9be-4eb1-4324-9588-ec7ac341aaf4"
+  tenant_id       = "9b517f7e-90ac-4870-ace7-a6bafa36c304"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
+  tags     = "${var.tags}"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+  tags                = "${var.tags}"
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = "${azurerm_resource_group.main.name}"
+  virtual_network_name = "${azurerm_virtual_network.main.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_public_ip" "main" {
+  name                         = "${var.prefix}-publicip"
+  resource_group_name          = "${azurerm_resource_group.main.name}"
+  location                     = "${azurerm_resource_group.main.location}"
+  allocation_method = "Static"
+  tags                         = "${var.tags}"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${azurerm_resource_group.main.location}"
+  resource_group_name = "${azurerm_resource_group.main.name}"
+
+  ip_configuration {
+    name                          = "configuration"
+    subnet_id                     = "${azurerm_subnet.internal.id}"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.main.id}"
+  }
+}
