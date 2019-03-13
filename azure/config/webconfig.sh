d82dc41eab782b0841128c0a18a2ad5668a40f52
gcastill0 <gilberto.castillo@rogers.com>
2019-03-13 14:31:00 -0400

    Adding simple Web assets

--- azure/config/webconfig.sh
+++ azure/config/webconfig.sh
@@ -2,3 +2,8 @@
 
 apt-get -y update
 apt-get -y install nginx
+
+sudo rm -f /var/www/html/*
+sudo wget https://raw.githubusercontent.com/gcastill0/hc-stack/master/azure/web/index.nginx-debian.html -P /var/www/html
+sudo wget https://raw.githubusercontent.com/gcastill0/hc-stack/master/azure/web/chaos.js -P /var/www/html
+sudo wget https://raw.githubusercontent.com/gcastill0/hc-stack/master/azure/web/lorenz1.js -P /var/www/html
