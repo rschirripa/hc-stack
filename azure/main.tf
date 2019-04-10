5444dfb970293f85e286b13a87d38bfd1a06110a
Gilberto Castillo <gilberto.castillo@rogers.com>
2019-04-10 11:27:04 -0400

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -19,4 +19,5 @@ resource "azurerm_subnet" "main" {
   resource_group_name  = "${azurerm_resource_group.main.name}"
   virtual_network_name = "${azurerm_virtual_network.main.name}"
   address_prefix       = "10.0.2.0/24"
+  tags                 = "${var.tags}"
 }
